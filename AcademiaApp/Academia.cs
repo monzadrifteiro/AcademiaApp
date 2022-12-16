187b1fb08ffefffbcca34d5731e205cc1303a711
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2022-12-16 02:53:35 -0300

    Adicionar arquivos de projeto.

---
+++ AcademiaApp/Academia.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaApp
+{
+    class Academia
+    {
+        private string nome;
+        private string endereco;
+        private Esporte[] esportes;
+        private int contador;
+        public Academia(string nome, string endereco)
+        {
+            this.nome = nome;
+            this.endereco = endereco;
+            esportes = new Esporte[5];
+        }
+        public void Inserir(Esporte e)
+        {
+            if (contador < 5)
+            {
+                esportes[contador] = e;
+                contador++;
+            }
+        }
+        public Esporte[] Listar()
+        {
+            Esporte[] i = new Esporte[contador];
+            Array.Copy(esportes, i, contador);
+            return i;
+        }
+        //public double MediaMensalidade() {
+        //  
+        //}
+        public override string ToString()
+        {
+            return $"{nome} - {endereco}";
+        }
+    }
+    class Esporte
+    {
+        private string nome;
+        private string horarios;
+        private double mensalidade;
+        public Esporte(string nome, string horarios, double mensalidade)
+        {
+            this.nome = nome;
+            this.horarios = horarios;
+            this.mensalidade = mensalidade;
+        }
+        public override string ToString()
+        {
+            return $"{nome} / {horarios} / {mensalidade} R$";
+        }
+    }
+}
---
+++ AcademiaApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcademiaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Academia A = new Academia(txtAcadNome.Text, txtAcadEnde.Text);
+            txtEspo.Items.Add(A.ToString());
+            txtAcadNome.Text = String.Empty;
+            txtAcadEnde.Text = String.Empty;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Academia A = new Academia(txtAcadNome.Text, txtAcadEnde.Text);
+            Esporte infos = new Esporte(txtEspNome.Text, txtEspHora.Text, double.Parse(txtEspMens.Text));
+            A.Inserir(infos);
+            txtEspNome.Text = String.Empty;
+            txtEspHora.Text = String.Empty;
+            txtEspMens.Text = String.Empty;
+            
+            foreach (Esporte modalidades in A.Listar())
+            {
+                txtEspo.Items.Add(modalidades);
+            }
+        }
+    }
+}
